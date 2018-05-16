c6a51456c3c20e3d1116092148309f9d4f746e92
rafaelb@unicauca.edu.co <bebetina1234>
2018-05-15 21:14:20 -0500

    zoom modificado y archivo js externo
    Se han estructurado el documento usando la carpeta js para los archivos javascript y se ha modificado el zoom para hacer ubicar la ventana subre la ciudad.

---
+++ js/ubicacion.js
@@ -0,0 +1,40 @@
+//Este codigo de pruba lo tome de Google maps.
+//Se ha modificado e zoom para hacer enfasis en la ciudad.
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 2.4466152 , lng: -76.5981539},
+    zoom: 14.5
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
